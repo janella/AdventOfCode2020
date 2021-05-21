8e29dffd57226392784080992932534837ed9799
janella <janella.espinas@gmail.com>
2021-05-21 23:04:54 +1200

    Day 1 part 2, bruteforce plus notes

--- src/Day1/day1.js
+++ src/Day1/day1.js
@@ -6,16 +6,21 @@ var input = fs.readFileSync('./input.txt', 'utf8')
     .map(x => parseInt(x))
     .sort((a, b) => a - b); // 2020 is not that big, so sort asc
 
-console.log(input);
 for (let i = 0; i < input.length; i++) {
     for (let j = 1; j < input.length; j++) {
-        var possible = input[i] + input[j];
-        console.log(`${input[i]} + ${input[j]} = ${possible}`);
-        if (target == possible) {
-            console.log(`Values are ${input[i]} and ${input[j]}!`);
-            var solution = input[i] * input[j];
-            console.log(`Solution is ${solution}.`);
-            return;
+        for (let k = 2; k < input.length; k++) {
+            var possible = input[i] + input[j] + input[k];
+            console.log(`${input[i]} + ${input[j]} + ${input[k]} = ${possible}`);
+            if (target == possible) {
+                console.log(`Values are ${input[i]}, ${input[j]}, and ${input[k]}!`);
+                var solution = input[i] * input[j] * input[k];
+                console.log(`Solution is ${solution}.`);
+                return;
+            }
         }
     }
 }
+
+// possible optimisations
+// - if the result is greater than 2020, skip over the rest of the `k` loop
+// - don't add the same value
