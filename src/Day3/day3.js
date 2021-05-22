65da1bf44789b184f35b3e20b1b2caca602ba264
janella <janella.espinas@gmail.com>
2021-05-23 10:21:50 +1200

    Day 3 Part 1 | Pseudocode, tests, vague JS noob code structure

--- src/Day3/day3.js
+++ src/Day3/day3.js
@@ -1,4 +1,62 @@
-function sum(a, b) {
-  return a + b;
+const fs = require('fs');
+class Day3 {
+    xMove = 3;
+    yMove = 1;
+
+    // input length 32
+    // num rows     323
+    // movement     right 3, down 1
+    // if tree      X
+    // if notTree   O
+    // result       numTrees
+
+    static solve() {
+        var input = fs.readFileSync('./input.txt', 'utf8')
+            .split('\r\n');
+        var numRows = input.length;
+
+        // track numTrees = 0
+        // get first loc
+        // while y < numRows; 
+        //  y = y + yMove
+        //  x = x + xMove
+        //  location = get next location
+        //  if location.value == tree
+        //      numTrees++;
+    }
+
+    static getXPosition(xMove, numPosition) {
+        // xMove * numPosition (eg, the third x location)
+    }
+
+    static getYPosition(yMove, numPosition) {
+        // yMove * numPosition (eg, the third y location)
+    }
+
+    static getLocationValues(rows, xValue, yValue){
+        // row = getYRowValues(rows, yValue)
+        // getXLocationValue(row, xValue)
+    }
+
+    static getYRowValues(rows, yValue) {
+        // splitRow = rows[yValue].split('')
+        // return splitRow
+    }
+
+    static getXLocationValue(row, xValue) {
+        // get row index
+        // location = splitRow[rowIndex]
+    }
+
+    static getRowIndex(rowLength, xValue) {
+        // expected x value
+        // rowIndex = x % row.length
+        // return rowIndex
+    }
+
+    static isTree(value) {
+        return value === '#';
+    }
 }
-module.exports = sum;
+
+module.exports = Day3;
--- src/Day3/day3.test.js
+++ src/Day3/day3.test.js
@@ -1,5 +1,38 @@
-const sum = require("./day3");
+const Day3 = require("./day3");
 
-test("Sum should work", () => {
-    expect(sum(1, 2)).toBe(3);
+// test - rows repeat horizontally - eg row[0] to row[31] == row[length + 1] to row[length * 2]
+// pattern repeats approx 10 times
+
+test("Should recognise tree", () => {
+    expect(Day3.isTree('#')).toBe(true);
+});
+
+test("Should generate correct coordinates", () => {
+    const xMove = 5;
+    const yMove = 2;
+    var numPosition = 3;
+    // get the third position
+    expect(Day3.getXPosition(xMove, numPosition)).toBe(15);
+    expect(Day3.getYPosition(yMove, numPosition)).toBe(6);
+});
+
+test("Should generate correct x value", () => {
+    var row = "12345678901234567890"; // 20 length
+    var length = row.length;
+    expect(Day3.getRowIndex(length, 25)).toBe(5);
+});
+
+test("Should get correct row", () => {
+    var rows = ["AAAA", "BBBB", "CCCC"];
+    var expectedY = 3;
+    expect(Day3.getYRowValues(rows, expectedY))
+        .toBe(['C', 'C', 'C', 'C']);
+});
+
+test("Should get correct location value", () => {
+    var rows = ["ABCD", "EFGH", "IJKL"];
+    var expectedX = 2;
+    var expectedY = 3;
+    expect(Day3.getLocationValues(rows, expectedX, expectedY))
+        .toBe('J');
 });
